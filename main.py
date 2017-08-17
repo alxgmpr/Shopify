e3d9d9d58cfc4523f3b468b4054eb66e4b413b43
alxgmpr <agompper@me.com>
2017-08-17 14:11:50 -0500

    changed thread loading

--- main.py
+++ main.py
@@ -3,19 +3,20 @@ import os
 from classes.shopify import Shopify
 from classes.logger import Logger
 
-log = Logger().log
-
 
 def main():
+    log = Logger('M').log
     threads = []
     i = 0
-    for config in os.listdir('configs'):
-        if config != "config.example.json":
-            log('MAIN', 'loading thread {} with config {}'.format(i, config))
+    files = os.listdir('configs')
+    for config in files:
+        if config == 'config.example.json':
+            log('skipping config example file')
+        else:
+            log('loading thread {} with config {}'.format(i, config))
             threads.append(Shopify('configs/' + config, i))
             threads[i].start()
             i += 1
-        else:
-            raise Exception('rename config.example.json')
+
 if __name__ == '__main__':
     main()
