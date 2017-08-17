8f1caedc988ad0cefb268a6a7e451cd2ecec0ffc
alxgmpr <agompper@me.com>
2017-08-17 14:10:39 -0500

    added variant to product class

--- classes/product.py
+++ classes/product.py
@@ -1,4 +1,14 @@
+from variant import Variant
+
+
 class Product(object):
     def __init__(self, name, url):
         self.name = name
         self.url = url
+        self.variants = []
+
+    def add_var_by_parts(self, vid, size):
+        self.variants.append(Variant(vid, size))
+
+    def add_var(self, var):
+        self.variants.append(var)
