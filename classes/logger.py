d03d35c7336a4dba3c8fcb71307b9aa560d9f305
alxgmpr <agompper@me.com>
2017-08-17 14:10:17 -0500

    cleaning up logger

--- classes/logger.py
+++ classes/logger.py
@@ -2,9 +2,10 @@ from datetime import datetime
 
 
 class Logger:
-    def __init__(self):
+    def __init__(self, tid):
         self.format = '%H:%M:%S.%f'
+        self.tid = tid
 
-    def log(self, tid, text):
+    def log(self, text):
         now = datetime.now().strftime(self.format)[:-4]
-        print '[t-{}] :: [{}] :: {}'.format(tid, now, text)
+        print '[{}] :: [{}] :: {}'.format(self.tid, now, text)
